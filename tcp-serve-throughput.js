acfa3ee36be29b705381bbea971d13b703ebd4b8
Brian Anderson <banderson@mozilla.com>
2013-05-06 23:08:45 -0700

    tcp-serve-throughput

---
+++ tcp-serve-throughput.js
@@ -0,0 +1,33 @@
+var net = require('net');
+
+var HOST = '127.0.0.1';
+var PORT = 9001;
+var msg_piece = "0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmno";
+var msg = "";
+for (var i = 0; i < 1024; i++) {
+    msg += msg_piece;
+}
+
+var msgbuf = new Buffer(msg, 'ascii');
+
+var NUM_WRITES = 64 * 1024;
+
+var server = net.createServer();
+server.on('connection', function(sock) {
+
+    writemsg(0, sock);
+
+    function writemsg(i, sock) {
+	if (i == NUM_WRITES) {
+	    console.log("wrote " + msgbuf.length * NUM_WRITES + " bytes");
+	    sock.destroy();
+	    server.close(0);
+	    return;
+	}
+
+	sock.write(msgbuf, function() {
+	    writemsg(i + 1, sock);
+	});
+    }
+});
+server.listen(PORT, HOST);
--- tcp-serve-throughput.rs
+++ tcp-serve-throughput.rs
@@ -5,6 +5,23 @@ use core::rt::io::*;
 use core::rt::io::net::tcp::TcpListener;
 use core::rt::io::net::ip::Ipv4;
 
+macro_rules! rtdebug (
+    ($( $arg:expr),+) => ( {
+        dumb_println(fmt!( $($arg),+ ));
+
+        fn dumb_println(s: &str) {
+            use core::io::WriterUtil;
+            let dbg = ::libc::STDERR_FILENO as ::io::fd_t;
+            dbg.write_str(s);
+            dbg.write_str("\n");
+        }
+
+    } )
+)
+
+static BUF_SIZE: uint = 64 * 1024;
+static NUM_WRITES: uint = 64 * 1024;
+
 #[start]
 fn start(_argc: int, _argv: **u8, _crate_map: *u8) -> int {
     let loop_ = ~UvEventLoop::new();
@@ -19,8 +36,9 @@ fn serve() {
     let addr = Ipv4(127, 0, 0, 1, 9001);
     let mut listener = TcpListener::bind(addr).unwrap();
     let mut stream = listener.accept().unwrap();
-    let buf = vec::from_fn(64 * 1024, |i| i as u8);
-    for 1024.times {
+    let buf = vec::from_fn(BUF_SIZE, |i| (i % 64 + 48) as u8);
+    for NUM_WRITES.times {
         stream.write(buf);
     }
+    rtdebug!("wrote %u bytes", buf.len() * NUM_WRITES);
 }
